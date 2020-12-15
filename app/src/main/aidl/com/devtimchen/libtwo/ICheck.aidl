a1a321365f27e6fdf7a5e398400e3d9d1b12d1ba
TimChen <tim@riversense.com>
2020-12-15 17:11:21 +0800

    feat: import lib one

--- app/src/main/aidl/com/devtimchen/libtwo/ICheck.aidl
+++
@@ -1,3 +0,0 @@
-interface ICheck {
-  void onCheck();
-}
--- app/src/main/java/com/devtimchen/libtwo/MainActivity.kt
+++ app/src/main/java/com/devtimchen/libtwo/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.devtimchen.libtwo
 
-import ICheck
+//import ICheck
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.devtimchen.libone.ICheck
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +12,6 @@ class MainActivity : AppCompatActivity() {
 
         // 建立一個 aidl 的 binder
         val binder = ICheck.Stub.asInterface(null)
+//        ICheck
     }
 }
